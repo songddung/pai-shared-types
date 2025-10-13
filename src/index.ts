632446ad826059ef978ca7c247ea12f4667377ad
songhyunkwang <shk9521@naver.com>
2025-10-14 00:23:51 +0900

    fix: export index

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ export * from "./dto/user/signup.response.dto";
 export * from "./dto/user/login.request.dto";
 export * from "./dto/user/login.response.dto";
 
-export * from "@dto/profile/create-profile-request.dto";
-export * from "@dto/profile/create-profile-response.dto";
+export * from "./dto/profile/create-profile-request.dto";
+export * from "./dto/profile/create-profile-response.dto";
 
 export * from "./dto/quiz/create-quiz.request.dto";
 export * from "./dto/quiz/create-quiz.response.dto";
